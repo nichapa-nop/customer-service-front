a8db2fdbb081cf5d6e5d9755bbc027517d62fd4a
nichapa-nop <165654040+nichapa-nop@users.noreply.github.com>
2024-06-27 18:25:01 +0700

    complete: login page(without error)

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,6 +3,8 @@
 import "./login.css";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
+import seenpic from "../../../../img/SV_SEEN-C_.png";
+import bgpic from "../../../../img/Rectangle 3.png";
 import toast, { Toaster } from "react-hot-toast";
 import { login } from "@/actions/login.action";
 
@@ -24,78 +26,81 @@ export default function loginPage() {
         // position: "top-right",
       });
     } else {
-      toast.error(loginResponse.data.message, {
-        //   position: "top-right",
-      });
+      // toast.error(loginResponse.data.message, {
+      //   //   position: "top-right",
+      // });
     }
   }
 
   return (
-    <div className="h-screen  bg-gradient-to-r from-orange-300 to-red-500">
+    <div className=" bg-gradient-to-tr from-deep-blue via-fade-purple to-bright-red h-screen ">
       <div className="flex items-center justify-center min-h-screen">
-        <div className="w-[400px] h-[500px] bg-white shadow-lg rounded-lg p-8 items-center justify-center">
-          <div className="text-center mt-4 mb-5">
-            <p className=" font-bold text-[32px]">Login</p>
-          </div>
-          <div>
-            <div className=" flex flex-col h-[360px] rounded-md p-3 ">
-              <div className="flex flex-col  h-[90px] p-4">
-                <p className=" text-[16px] ">Email</p>
+        <div className="flex flex-row bg-white h-[529px] w-[923px] rounded-lg overflow-hidden">
+          <div className=" flex flex-col w-[462px] p-12">
+            <div className=" flex mt-2 justify-center">
+              <img
+                src={seenpic.src}
+                alt="image"
+                className=" w-[137px] h-[95px]"
+              ></img>
+            </div>
+            <div className=" mt-4 justify-center items-center text-center">
+              <p className=" bg-gradient-to-tr from-deep-blue via-fade-purple to-bright-red   inline-block text-transparent bg-clip-text font-medium">
+                Sign in
+              </p>
+            </div>
+            <div className=" flex flex-col  mt-1 ">
+              <span>Email</span>
+              <div className="bg-dark-gray hover:bg-gradient-to-tr from-deep-blue via-fade-purple to-bright-red p-[2px]  mt-1 rounded-[15px]">
                 <input
-                  type="text"
                   id="email"
-                  className=" bg-gray-100 w-[280px] h-[26px] mt-2 rounded-[4px] text-[12px]"
-                  placeholder="Enter your Email"
+                  type="text"
+                  className=" w-full h-[48px] rounded-[13px] pl-4"
+                  placeholder="example.ee@baseplayhouse.co"
                   onChange={(e) => {
                     e.preventDefault();
                     setEmail(e.target.value);
                   }}
                   value={email}
-                />
+                ></input>
               </div>
-              <div className="flex flex-col  h-[90px] p-4">
-                <p className="text-[16px]">Password</p>
+            </div>
+            <div className=" flex flex-col  mt-[14px] ">
+              <span>Password</span>
+              <div className="bg-dark-gray hover:bg-gradient-to-tr from-deep-blue via-fade-purple to-bright-red  mt-1 p-[2px]  rounded-[15px]">
                 <input
                   type="password"
                   id="password"
-                  className=" bg-gray-100 w-[280px] h-[26px]  mt-2 rounded-[4px] text-[12px]"
-                  placeholder="Enter your Password"
+                  className=" w-full h-[48px] rounded-[13px] pl-5"
+                  placeholder="● ● ● ● ● ● ● ● ● ● ● ●"
                   onChange={(e) => {
                     e.preventDefault();
                     setPassword(e.target.value);
                   }}
                   value={password}
-                />
-              </div>
-              <div className="flex flex-col  h-[30px] items-end p-3">
-                <a
-                  type="button"
-                  className=" text-gray-600 border border-transparent  hover:opacity-75 text-[12px] hover:cursor-pointer"
-                  href="http://localhost:3000/forgotpassword"
-                  //   onClick={() => router.push("/forgotpassword")}
-                >
-                  forgot password?
-                </a>
-              </div>
-              <div className=" flex  h-[70px] items-center justify-center p-2 mt-2">
-                <button
-                  className=" hover:opacity-75 h-[50px] w-full bg-gradient-to-r from-orange-300 to-red-500  rounded-[30px]"
-                  onClick={handleLogin}
-                >
-                  <p className=" text-white font-semibold text-[15px] ">
-                    LOGIN
-                  </p>
-                </button>
-              </div>
-              <div className="flex flex-col mt-8 items-end">
-                <a
-                  className="hover:opacity-75 text-gray-600 text-[14px]"
-                  href="http://localhost:3000/createaccount"
-                >
-                  Create your Account →
-                </a>
+                ></input>
               </div>
             </div>
+            <div className=" flex justify-end mt-2">
+              <button className="bg-gradient-to-tr from-deep-blue via-fade-purple to-bright-red  inline-block text-transparent bg-clip-text font-medium">
+                Forgot Password?
+              </button>
+            </div>
+            <div className=" mt-5 text-center ">
+              <button
+                className="bg-gradient-to-tr from-deep-blue via-fade-purple to-bright-red w-full rounded-[15px] h-12 text-white text-md "
+                onClick={handleLogin}
+              >
+                Sign In
+              </button>
+            </div>
+          </div>
+          <div className="bg-gradient-to-tr from-deep-blue via-fade-purple to-bright-red w-[461px] h-[529px] overflow-hidden">
+            <img
+              src={bgpic.src}
+              alt="image"
+              className="w-full h-full opacity-60"
+            ></img>
           </div>
         </div>
         <Toaster position="bottom-center" />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        "deep-blue": "#1f1a4f",
+        "fade-purple": "#82303d",
+        "bright-red": "#ec4723",
+        "dark-gray": "#9F9F9F",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {},
     },
   },
   plugins: [],
