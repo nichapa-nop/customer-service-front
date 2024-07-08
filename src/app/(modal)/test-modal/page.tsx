83fd13373664c939a297d5db733ceb6347e7fe05
nichapa-nop <165654040+nichapa-nop@users.noreply.github.com>
2024-07-08 11:24:28 +0700

    Update page.tsx

--- src/app/(modal)/test-modal/page.tsx
+++ src/app/(modal)/test-modal/page.tsx
@@ -82,11 +82,14 @@ import {
   ModalBody,
   ModalFooter,
   Button,
-  // useDisclosure,
+  useDisclosure,
+  RadioGroup,
+  Radio,
 } from "@nextui-org/react";
 
 export default function App() {
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+
   // const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   return (
@@ -98,17 +101,48 @@ export default function App() {
       >
         Open Modal
       </button>
-      <Modal isOpen={isOpenModal} onOpenChange={setIsOpenModal}>
-        <ModalContent className=" bg-red-300 m-10 p-3">
+
+      <Modal
+        isOpen={isOpenModal}
+        onOpenChange={setIsOpenModal}
+        className="h-[2200px] w-[1000px]"
+      >
+        <ModalContent className=" bg-red-300 m-10 p-3 rounded-[15px]">
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1 text-center">
+              <ModalHeader className="flex flex-col gap-1 text-center ">
                 Create Ticket
               </ModalHeader>
-              <ModalBody className="bg-yellow-200">
-                <div className="bg-blue-300 ">Customer Info</div>
-                <div className=" bg-pink-300">System Info</div>
-                <div className=" bg-green-300">General Info</div>
+              <ModalBody className="bg-yellow-200 p-1">
+                <div className="grid grid-cols-2 gap-2 bg-blue-300 p-4  rounded-xl">
+                  <p className=" col-span-2">Customer Info</p>
+                  <div className=" bg-red-200">
+                    <p>firstname</p>
+                    <input placeholder="ABC"></input>
+                  </div>
+                  <div className="bg-lime-200">
+                    <p>lastname</p>
+                    <input placeholder="DEF"></input>
+                  </div>
+                  <div className="bg-lime-200">
+                    <p>Phone Number</p>
+                    <input placeholder="0123456789"></input>
+                  </div>
+                  <div className=" bg-red-200">
+                    <p>Type</p>
+                    <input placeholder="Select"></input>
+                  </div>
+                  <div className=" bg-red-200">
+                    <p>Email</p>
+                    <input placeholder="ee@baseplayhouse.co"></input>
+                  </div>
+                  <div className=" bg-lime-200">
+                    <p>Company Name</p>
+                    <input placeholder="BASE PLAYHOUSE CO. TLD."></input>
+                  </div>
+                </div>
+                <div className=" bg-pink-300 h-[420px]">System Info</div>
+                <div className=" bg-green-300 h-[480px]">General Info</div>
                 <div className=" bg-cyan-300">CS Info</div>
               </ModalBody>
               <ModalFooter>
