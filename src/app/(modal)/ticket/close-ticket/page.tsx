83a268e0f11e4d6ca33f92f32036a85015e4669d
nichapa-nop <165654040+nichapa-nop@users.noreply.github.com>
2024-07-17 10:57:32 +0700

    create modal folder

--- src/app/(modal)/ticket/close-ticket/page.tsx
+++ src/app/(modal)/ticket/close-ticket/page.tsx
@@ -2,6 +2,7 @@
 import {
   Description,
   Dialog,
+  DialogBackdrop,
   DialogPanel,
   DialogTitle,
 } from "@headlessui/react";
@@ -18,9 +19,11 @@ export default function Example() {
         onClose={() => setIsOpen(false)}
         className="relative z-50"
       >
+        <DialogBackdrop className="fixed inset-0 bg-black/30" />
+
         <div className="fixed inset-0 w-screen overflow-y-auto p-1">
           <div className="flex min-h-full  items-center justify-center">
-            <DialogPanel className=" w-[920px] h-[920px] space-y-[50px] border rounded-xl bg-red-300 p-12 ">
+            <DialogPanel className=" bg-white w-[920px] h-[920px] space-y-[50px] border rounded-xl p-12 ">
               <DialogTitle className="font-semibold text-center text-[20px]">
                 Close Ticket
               </DialogTitle>
@@ -28,30 +31,30 @@ export default function Example() {
                 This will permanently deactivate your account
               </Description> */}
 
-              <div className=" bg-blue-300 p-6 h-[630px] rounded-xl">
+              <div className=" bg-white shadow-md shadow-light-gray1 p-6 h-[630px] rounded-xl">
                 <p className="font-semibold text-[20px] pl-6">Customer Info</p>
-                <div className="  bg-slate-500 p-3">
-                  <div className="flex flex-col gap-2 bg-red-200 p-4">
-                    <p>Solution</p>
+                <div className=" p-3">
+                  <div className="flex flex-col gap-2 p-4">
+                    <p className="font-medium">Solution</p>
                     <input
-                      className=" w-full h-[250px] rounded-[15px] pl-4 hover:placeholder:text-space-black"
+                      className=" bg-light-gray2 w-full h-[250px] rounded-[15px] pl-4 hover:placeholder:text-space-black "
                       placeholder="Solution"
                     ></input>
                   </div>
-                  <div className="flex flex-col gap-2 bg-red-200 p-4">
-                    <p>Inform the customer</p>
-                    <div className=" bg-yellow-300 w-full h-[150px] rounded-[15px] hover:placeholder:text-space-black">
-                      <div className="flex bg-orange-400 items-center mt-1 space-x-3">
+                  <div className="flex flex-col gap-2 p-4">
+                    <p className="font-medium">Inform the customer</p>
+                    <div className="  w-full h-[150px] rounded-[15px] hover:placeholder:text-space-black">
+                      <div className="flex items-center mt-1 space-x-3">
                         <input
                           type="checkbox"
                           className="w-[27px] h-[27px]"
                         ></input>
                         <span>Send email to Inform the customer</span>
                       </div>
-                      <div className=" flex flex-col gap-2 bg-red-500 p-4 pl-10">
-                        <p>Customer Email</p>
+                      <div className=" flex flex-col gap-2 p-4 pl-10">
+                        <p className="font-medium">Customer Email</p>
                         <input
-                          className=" w-full h-10 rounded-[15px] pl-4 hover:placeholder:text-space-black"
+                          className=" bg-light-gray2 w-full h-10 rounded-[15px] pl-4 hover:placeholder:text-space-black"
                           placeholder="example.ee@baseplayhouse.co"
                         ></input>
                       </div>
